21186d4a7fae303c1afff8472175373a4c88ce15
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-03-30 20:17:42 -0300

    LTS-19
    await async function

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,22 +1,16 @@
 import axios from "axios";
 import { AuthData } from "../types/AuthData";
 
-const URL_API = process.env.REACT_APP_API_URL;
-
-console.log(URL_API);
-
 const api = axios.create({
-    baseURL: URL_API,
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const auth = async (data: AuthData) => {
     try {
-        debugger
-        const response = api.post('/users/auth', { email: data.email, password: data.password });
+        const response = await api.post('/users/auth', { email: data.email, password: data.password });
         console.log(response);
         return response;
     } catch (err) {
-        debugger;
         console.error(err);
         return null;
     }
