235e5676d2ea7acab4108b5682c770144b6b2710
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-03-30 22:12:33 -0300

    LTS-19
    Headers request

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,6 +3,11 @@ import { AuthData } from "../types/AuthData";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Accept-Encoding": "gzip, deflate, br"
+    }
 });
 
 export const auth = async (data: AuthData) => {
