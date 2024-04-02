6fee8130f8876fe0915e4e843d2697dec382f502
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-04-02 13:02:01 -0300

    LST-20

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Home from "../components/Home";
 import Login from "./user/Login";
 import Dashboard from "./Dashboard";
 import ProjectsOwnerList from "./projects/OwnerList";
+import Register from "./user/Register";
 
 const AppRoutes = () => {
 
@@ -12,6 +13,7 @@ const AppRoutes = () => {
                 {/* Public Access */}
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/login" element={<Login />}></Route>
+                <Route path="/register" element={<Register />}></Route>
                 {/* Private Access */}
                 <Route path="/dashboard" element={<Dashboard />}></Route>
                 <Route path="/my-owner-projects" element={<ProjectsOwnerList />}></Route>
