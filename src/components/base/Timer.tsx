2072d8d38c148d28fb64f35ec10e8ed92d74d290
André Cristen <30880513+andrecristen@users.noreply.github.com>
2024-05-13 23:04:14 -0300

    LST-38
    Screen record

--- src/components/base/Timer.tsx
+++ src/components/base/Timer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FiCheck, FiCheckSquare, FiPlay, FiSkipBack } from 'react-icons/fi';
+import { FiCheckSquare, FiPlay, FiSkipBack, FiVideo } from 'react-icons/fi';
 
 interface TimerProps {
     title: string;
@@ -12,7 +12,9 @@ interface TimerProps {
 const Timer: React.FC<TimerProps> = ({ title, onChange, onStart, onStop, onReset }) => {
     const [isRunning, setIsRunning] = useState(false);
     const [timeElapsed, setTimeElapsed] = useState(0);
-    const [isInitial, setIsInitial] = useState(true); // Adicionamos uma variável para controlar se o temporizador está no estado inicial ou não
+    const [isInitial, setIsInitial] = useState(true);
+    const [isRecording, setIsRecording] = useState(false);
+    const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
 
     let intervalId: NodeJS.Timeout;
 
@@ -30,11 +32,11 @@ const Timer: React.FC<TimerProps> = ({ title, onChange, onStart, onStop, onReset
         }
 
         return () => clearInterval(intervalId);
-    }, [isRunning, onChange]);
+    }, [isRunning, isRecording, onChange]);
 
     const handleStart = () => {
         setIsRunning(true);
-        setIsInitial(false); // Quando o temporizador é iniciado, ele não está mais no estado inicial
+        setIsInitial(false);
         if (onStart) {
             onStart();
         }
@@ -42,7 +44,8 @@ const Timer: React.FC<TimerProps> = ({ title, onChange, onStart, onStop, onReset
 
     const handleStop = () => {
         setIsRunning(false);
-        setIsInitial(true); // Quando o temporizador é parado, ele volta ao estado inicial
+        setIsInitial(true);
+        stopRecorder();
         if (onStop) {
             onStop();
         }
@@ -51,10 +54,50 @@ const Timer: React.FC<TimerProps> = ({ title, onChange, onStart, onStop, onReset
     const handleReset = () => {
         setTimeElapsed(0);
         setIsRunning(false);
-        setIsInitial(true); // Quando o temporizador é reiniciado, ele volta ao estado inicial
+        setIsInitial(true);
         onChange(0);
+        stopRecorder();
         if (onReset) {
             onReset();
+        }
+    };
+
+    const stopRecorder = () => {
+        if (mediaRecorder && mediaRecorder.state === 'recording') {
+            mediaRecorder.stop();
+            setIsRecording(false);
+        }
+    }
+
+    const handleRecord = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getDisplayMedia();
+
+            const recorder = new MediaRecorder(stream);
+            setMediaRecorder(recorder);
+
+            const chunks: Blob[] = [];
+
+            recorder.ondataavailable = (event) => {
+                chunks.push(event.data);
+            };
+
+            recorder.onstop = () => {
+                const blob = new Blob(chunks, { type: 'video/webm' });
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                document.body.appendChild(a);
+                a.href = url;
+                a.download = `Gravação de Tela - ${Date.now()}.webm`;
+                a.click();
+                window.URL.revokeObjectURL(url);
+                setIsRecording(false);
+            };
+
+            recorder.start();
+            setIsRecording(true);
+        } catch (error) {
+            console.error('Erro ao iniciar a gravação:', error);
         }
     };
 
@@ -74,12 +117,17 @@ const Timer: React.FC<TimerProps> = ({ title, onChange, onStart, onStop, onReset
                 </button>
             ) : (
                 <>
-                    <button onClick={handleStop} className="action-button-green m-2">
+                    <button onClick={handleStop} className="action-button-red m-2">
                         <FiCheckSquare className="m-2 text-2xl" /> Finalizar
                     </button>
-                    <button onClick={handleReset} className="action-button-red m-2">
+                    <button onClick={handleReset} className="action-button-teal m-2">
                         <FiSkipBack className="m-2 text-2xl" /> Reiniciar
                     </button>
+                    {!isRecording && (
+                        <button onClick={handleRecord} className="action-button-purple m-2">
+                            <FiVideo className="m-2 text-2xl" /> Gravar Tela
+                        </button>
+                    )}
                 </>
             )}
         </div>
